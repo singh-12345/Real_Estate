8ee791c4a90c021ee7c18faac689cca0ae2046b3
singh- 12345 <amansinghania16@gmail.com>
2024-08-30 21:03:56 +0530

    third

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.r.js'
 import authRouter from './routes/auth.r.js'
 import listingRouter from './routes/listing.r.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then( () => {
@@ -14,7 +15,7 @@ mongoose.connect(process.env.MONGO).then( () => {
   console.log(err)
 });
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -27,16 +28,18 @@ app.listen(3000, () => {
 });
 
 
-// app.use(express.static(path.join(__dirname, '/client/dist')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// })
 
 // API ROUTES
 app.use('/server/user',userRouter);
 app.use('/server/auth',authRouter);
 app.use('/server/listing',listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 // MIDDLEWARE
